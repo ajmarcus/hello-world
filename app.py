77604fdc69dc4c9268c4b3c2bd699f8daf09bb39
Michael Burt <michael@mpburt.com>
2020-09-27 18:51:44 -0600

    update

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return f"Hello {os.environ.get('NAME')}"
+    return f"Hello {os.environ.get('NAME')}!!!"
 
 if __name__ == "__main__":
     app.run()
